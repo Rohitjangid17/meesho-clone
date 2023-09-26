931b331984aedd9dec257f3a1f6340054813b634
monu_jangid76 <jangidrohit817@gmail.com>
2023-09-26 12:56:51 +0530

    add some component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './layout/login/login.component';
+import { LoginComponent } from './shared/auth/login/login.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
