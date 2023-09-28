3727cc06e8cfe798a65f16249943cc1474072e30
monu_jangid76 <jangidrohit817@gmail.com>
2023-09-28 23:38:17 +0530

    login and signup form password input value visible and hidden functionality done

--- src/app/shared/auth/auth.component.ts
+++ src/app/shared/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -7,8 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AuthComponent {
   formScreen: boolean = false;
+  signupForm: FormGroup;
+  loginForm: FormGroup;
+  isSignupPassword: boolean = false;
+  isLoginPassword: boolean = false;
+
+  constructor(
+    private _formBuilder: FormBuilder
+  ) {
+    // signup form group
+    this.signupForm = this._formBuilder.group({
+      email: ["", [Validators.required]],
+      mobileNumber: ["", [Validators.required]],
+      password: ["", [Validators.required]],
+    });
+
+    // login form group
+    this.loginForm = this._formBuilder.group({
+      mobileNumber: ["", [Validators.required]],
+      password: ["", [Validators.required]],
+    })
+  }
+
+  signup() {
+    console.log(this.signupForm.value);
+  }
+
+  login() {
+    console.log(this.loginForm.value);
+  }
 
   authToggleScreen() {
     return this.formScreen = this.formScreen ? false : true;
+  }
+
+  toggleSignupPassword() {
+    return this.isSignupPassword = this.isSignupPassword ? false : true;
+  }
+
+  toggleLoginPassword() {
+    return this.isLoginPassword = this.isLoginPassword ? false : true;
   }
 }
--- src/app/shared/auth/auth.module.ts
+++ src/app/shared/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { AuthRoutingModule } from "./auth-routing.module";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from '@angular/material/input';
-
-import { AuthComponent } from "./auth.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
+import { MatIconModule } from '@angular/material/icon';
+
+import { AuthComponent } from "./auth.component";
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { CommonModule } from "@angular/common";
         MatFormFieldModule,
         MatInputModule,
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        MatIconModule
     ],
     providers: [],
 })
