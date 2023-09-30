f0627b7e5359b51acde31ea05c5593ddaf35b362
monu_jangid76 <jangidrohit817@gmail.com>
2023-09-30 12:49:06 +0530

    add header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Signin } from 'src/app/shared/interfaces/data-type';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  userName: string = "";
 
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    let user = localStorage.getItem("user");
+
+    if (user) {
+      this.userName = user && JSON.parse(user)[0].name;
+      console.log(this.userName)
+    }
+  }
 }
